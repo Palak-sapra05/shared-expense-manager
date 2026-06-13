3337e25c7d5a19c125207ad62d0b3098e3fcce31
Palak-sapra05 <saprapalak69@gmail.com>
2026-06-13 16:43:36 +0530

    Initialize Spring Boot backend

---
+++ backend/src/test/java/com/palak/expensemanager/ExpensemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palak.expensemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
